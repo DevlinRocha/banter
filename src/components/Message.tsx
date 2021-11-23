638dee5f760750d38d3ab015267607bb0192ed49
Devlin <devlincodes@gmail.com>
2021-11-22 18:58:39 -0800

    Basic responsive chat layout

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,5 @@
 import tw from "tailwind-styled-components";
+import Image from "next/image";
 
 interface MessageProps {
   message: {
@@ -7,6 +8,7 @@ interface MessageProps {
       username: string;
     };
     content: string;
+    date: string;
     edited: boolean;
     reactions: null;
   };
@@ -15,18 +17,51 @@ interface MessageProps {
 export default function Message(props: MessageProps) {
   return (
     <Container>
-      <Username>{props.message.user.username}</Username>
-      <ProfilePicture src={props.message.user.img} />
-      <p>{props.message.content}</p>
+      <ProfilePicture>
+        <StyledImage
+          loader={() => props.message.user.img}
+          src={props.message.user.img}
+          width={40}
+          height={40}
+          alt="Profile picture"
+        />
+      </ProfilePicture>
+      <MessageContent>
+        <MessageInfo>
+          <Username>{props.message.user.username}</Username>
+          <Date>{props.message.date}</Date>
+        </MessageInfo>
+        <Content>{props.message.content}</Content>
+      </MessageContent>
     </Container>
   );
 }
 
-const Container = tw.div`
+const Container = tw.li`
+  flex mt-2
 `;
 
-const ProfilePicture = tw.img`
+const ProfilePicture = tw.div`
+  absolute left-16px
 `;
 
-const Username = tw.p`
+const MessageContent = tw.div`
+  flex flex-col ml-72px
+`;
+
+const StyledImage = tw(Image)`
+  object-contain rounded-full
+`;
+
+const MessageInfo = tw.div`
+  flex gap-1
+`;
+
+const Username = tw.span`
+`;
+
+const Date = tw.span`
+`;
+
+const Content = tw.p`
 `;
