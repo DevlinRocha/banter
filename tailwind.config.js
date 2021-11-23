eda6e5eba2bff2390f4e919617a0914ae855d079
Devlin <devlincodes@gmail.com>
2021-11-22 18:57:39 -0800

    Added custom spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "16px": "16px",
+        "72px": "72px",
+      },
+    },
   },
   variants: {
     extend: {},
