f89bc3a4b3fb6020af346f5eb154b2d1e6b9944f
Devlin <devlincodes@gmail.com>
2021-11-24 19:37:04 -0800

    Messages can now be sent to the server

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,14 +1,47 @@
+import { useRef } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 import tw from "tailwind-styled-components";
 
 export default function TextArea() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  function getText() {
+    let messageContent;
+    if (inputRef.current) {
+      messageContent = inputRef.current.value;
+    }
+    return messageContent;
+  }
+  async function sendMessage(e: any) {
+    e.preventDefault();
+    console.log(Date());
+    try {
+      const docRef = await addDoc(
+        collection(db, "serverList", "public", "servers", "global", "messages"),
+        {
+          content: getText(),
+          date: Date(),
+          edited: false,
+          reactions: [],
+          user: {
+            img: "https://pbs.twimg.com/profile_images/1369982148682215427/dbS7T1nr_400x400.jpg",
+            name: "Devlin",
+          },
+        }
+      );
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
-    <Container>
-      <TextInput type="text" placeholder="Message" />
+    <Container onSubmit={sendMessage}>
+      <TextInput ref={inputRef} type="text" placeholder="Message" />
     </Container>
   );
 }
 
-const Container = tw.div`
+const Container = tw.form`
 
 `;
 
