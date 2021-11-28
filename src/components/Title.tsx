28987d888c43d69c43a1ad044fa1849d30844161
Devlin <devlincodes@gmail.com>
2021-11-27 23:10:38 -0800

    Added topic to server title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,27 @@
 import tw from "tailwind-styled-components";
 
 export default function Title() {
-  return <Header>Global Chat</Header>;
+  return (
+    <Section>
+      <Heading>Global Chat</Heading>
+      <Divider>|</Divider>
+      <Topic>Welcome to Banter</Topic>
+    </Section>
+  );
 }
 
-const Header = tw.section`
+const Section = tw.section`
   flex sticky top-0 h-10 bg-white z-10
+`;
+
+const Heading = tw.h3`
+  p-2
+`;
+
+const Divider = tw.div`
+  p-1.5
+`;
+
+const Topic = tw.span`
+  p-2
 `;
