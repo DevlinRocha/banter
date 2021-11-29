1effd115dc6f2aa4f9e39e371334190c612e801f
Devlin <devlincodes@gmail.com>
2021-11-28 21:17:56 -0800

    Added server channel list sidebar

---
+++ src/components/Channels.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { query, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
+import tw from "tailwind-styled-components/dist/tailwind";
+
+interface ChannelsProps {
+  server: string;
+}
+
+export default function Channels(props: ChannelsProps) {
+  const [channels, setChannels] = useState<any[]>([]);
+  useEffect(() => {
+    getChannels();
+  }, []);
+  async function getChannels() {
+    const channelList: any[] = [];
+    const q = query(collection(db, "servers", props.server, "channels"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      channelList.push(doc.id);
+    });
+    console.log(channelList);
+    setChannels(channelList);
+  }
+  return (
+    <Nav>
+      <Sidebar>
+        {channels.map((channel, index) => {
+          return (
+            <Link href="/" key={index}>
+              <Channel>{channel}</Channel>
+            </Link>
+          );
+        })}
+      </Sidebar>
+    </Nav>
+  );
+}
+
+const Nav = tw.nav`
+    hidden lg:block h-screen
+`;
+
+const Sidebar = tw.ol`
+`;
+
+const Channel = tw.li`
+  cursor-pointer
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from "next";
+import Channels from "../components/Channels";
 import Chat from "../components/Chat";
+import tw from "tailwind-styled-components/dist/tailwind";
 
 const Home: NextPage = () => {
-  return <Chat />;
+  return (
+    <Container>
+      <Channels server={"global"} />
+      <Chat />
+    </Container>
+  );
 };
+
+const Container = tw.div`
+  flex
+`;
 
 export default Home;
