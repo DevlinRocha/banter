6ab23b1649e366c6a6ea974bb03607e5f13f7824
Devlin <devlincodes@gmail.com>
2021-12-13 20:36:48 -0800

    Added dynamic rendering for user settings menu

--- src/pages/channels/[sid]/[cid].tsx
+++ src/pages/channels/[sid]/[cid].tsx
@@ -2,21 +2,45 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Servers from "../../../components/Servers";
 import Channels from "../../../components/Channels";
-import Chat from "../../../components/Chat";
+import Chat from "../../../components/chat/Chat";
+import UserSettings from "../../../components/userSettings/UserSettings";
 import tw from "tailwind-styled-components/dist/tailwind";
+import { auth } from "../../../../firebase";
+import { useSettingsState } from "../../../features/settings";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { sid, cid } = router.query;
+  const { userSettingsOpen } = useSettingsState();
 
   return (
     <Container>
-      <Servers />
-      <Channels />
-      <Chat />
+      {userSettingsOpen ? (
+        <UserSettings />
+      ) : (
+        <Container>
+          <Servers />
+          <Channels />
+          <Chat />
+        </Container>
+      )}
     </Container>
   );
 };
+
+export async function getServerSideProps() {
+  if (!auth) {
+    return {
+      redirect: {
+        destination: "/register",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 const Container = tw.div`
   flex
