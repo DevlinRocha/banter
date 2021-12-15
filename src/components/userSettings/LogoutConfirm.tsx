ad8a707711f7e387b52d29be2884288a6f586014
Devlin <devlincodes@gmail.com>
2021-12-14 19:54:13 -0800

    Created LogoutConfirm window

---
+++ src/components/userSettings/LogoutConfirm.tsx
@@ -0,0 +1,20 @@
+import tw from "tailwind-styled-components/dist/tailwind";
+
+export default function LogoutConfirm() {
+  return (
+    <Container>
+      <Heading>Log Out</Heading>
+      <Body>Are you sure you want to log out?</Body>
+    </Container>
+  );
+}
+
+const Container = tw.div`
+    fixed top-1/2 left-1/2 z-10
+`;
+
+const Heading = tw.h2`
+`;
+
+const Body = tw.p`
+`;
