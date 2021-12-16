f0438c528a35cd93b34045ff63442ac556ad0c74
Devlin <devlincodes@gmail.com>
2021-12-15 21:01:47 -0800

    Moved Auth observer to page level

--- src/components/userSettings/UserProfileCard.tsx
+++ src/components/userSettings/UserProfileCard.tsx
@@ -1,29 +1,13 @@
-import { useEffect } from "react";
 import { useAppDispatch } from "../../redux/hooks";
-import { useServersState, setUser } from "../../features/servers";
-import { auth } from "../../../firebase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useUserState } from "../../features/user";
+import { getAuth } from "firebase/auth";
 import tw from "tailwind-styled-components/dist/tailwind";
 import Image from "next/image";
 
 export default function UserProfileCard() {
-  const { user } = useServersState();
+  const auth = getAuth();
+  const { user } = useUserState();
   const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in
-        const currentUser = {
-          name: user.displayName,
-          img: user.photoURL,
-        };
-        dispatch(setUser(currentUser));
-      } else {
-        // User is signed out
-      }
-    });
-  }, []);
 
   return (
     <ContainerCol>
@@ -42,6 +26,7 @@ export default function UserProfileCard() {
 
         <Username>
           <DisplayName>{user.name}</DisplayName>
+
           <Discriminator>#{user.id}</Discriminator>
         </Username>
       </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Servers from "../components/Servers";
-import Channels from "../components/Channels";
+import Channels from "../components/channels/Channels";
 import Chat from "../components/chat/Chat";
 import UserSettings from "../components/userSettings/UserSettings";
 import tw from "tailwind-styled-components/dist/tailwind";
-import { auth } from "../../firebase";
+import { setUser } from "../features/user";
+import { useAppDispatch } from "../redux/hooks";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useSettingsState } from "../features/settings";
 
 const Home: NextPage = () => {
+  const auth = getAuth();
   const router = useRouter();
   const { sid, cid } = router.query;
   const { userSettingsOpen } = useSettingsState();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const currentUser = {
+          name: user.displayName,
+          img: user.photoURL,
+        };
+        dispatch(setUser(currentUser));
+      } else {
+        // User is signed out
+      }
+    });
+  }, []);
 
   return (
     <Container>
@@ -29,6 +49,8 @@ const Home: NextPage = () => {
 };
 
 export async function getServerSideProps() {
+  const auth = getAuth();
+
   if (!auth) {
     return {
       redirect: {
