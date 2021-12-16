1fd61297d65cdbbcee11e5f3e9dbabbdb7fdaf19
Devlin <devlincodes@gmail.com>
2021-12-16 15:23:31 -0800

    Added close button to user settings

--- src/components/userSettings/UserSettings.tsx
+++ src/components/userSettings/UserSettings.tsx
@@ -1,11 +1,15 @@
 import tw from "tailwind-styled-components/dist/tailwind";
-import { useSettingsState } from "../../features/settings";
+import { setUserSettingsOpen, useSettingsState } from "../../features/settings";
 import SettingsSidebar from "./SettingsSidebar";
 import SettingsView from "./SettingsView";
 import LogoutConfirm from "./LogoutConfirm";
+import Image from "next/image";
+import closeButton from "../../../assets/closeButton.svg";
+import { useAppDispatch } from "../../redux/hooks";
 
 export default function UserSettings() {
-  const { logoutConfirmOpen } = useSettingsState();
+  const { userSettingsOpen, logoutConfirmOpen } = useSettingsState();
+  const dispatch = useAppDispatch();
 
   return (
     <Container>
@@ -14,10 +18,33 @@ export default function UserSettings() {
       <SettingsSidebar />
 
       <SettingsView />
+
+      <CloseButton>
+        <StyledImage
+          onClick={() => dispatch(setUserSettingsOpen(!userSettingsOpen))}
+          src={closeButton}
+          width={36}
+          height={36}
+          alt={"Close button"}
+        />
+
+        <Caption>ESC</Caption>
+      </CloseButton>
     </Container>
   );
 }
 
 const Container = tw.div`
   flex w-screen h-screen
+`;
+
+const CloseButton = tw.figure`
+  pt-15 text-center
+`;
+
+const StyledImage = tw(Image)`
+  cursor-pointer
+`;
+
+const Caption = tw.figcaption`
 `;
