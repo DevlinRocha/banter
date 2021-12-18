676381f65df9e62c5bce5bdd63babb735a23af1c
Devlin <devlincodes@gmail.com>
2021-12-18 14:49:53 -0800

    Upgraded to Tailwind CSS v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
     extend: {
       spacing: {
         15: "3.75rem",
+
         18: "4.5rem",
+
         19: "4.75rem",
+
         25: "6.25rem",
+
         165: "41.25rem",
       },
     },
   },
-  variants: {
-    extend: {
-      borderRadius: ["hover"],
-    },
-  },
+
   plugins: [],
 };
