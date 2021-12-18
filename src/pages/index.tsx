bf657b74c39267049a710f04efa37c48c3132bca
Devlin <devlincodes@gmail.com>
2021-12-17 20:24:33 -0800

    Stores all user information in Redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,9 @@ import tw from "tailwind-styled-components/dist/tailwind";
 import { setUser } from "../features/user";
 import { useAppDispatch } from "../redux/hooks";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { useSettingsState } from "../features/settings";
+import { db } from "../../firebase";
 
 const Home: NextPage = () => {
   const auth = getAuth();
@@ -17,16 +19,24 @@ const Home: NextPage = () => {
   const { userSettingsOpen } = useSettingsState();
   const dispatch = useAppDispatch();
 
-  onAuthStateChanged(auth, (user) => {
-    if (user?.displayName && user?.photoURL) {
-      // User is signed in
-      const currentUser = {
-        name: user.displayName,
-        img: user.photoURL,
-      };
-      dispatch(setUser(currentUser));
-    } else {
-      // User is signed out
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const currentUser = {
+          username: docSnap.data().username,
+          avatar: docSnap.data().avatar,
+          tag: docSnap.data().tag,
+          about: docSnap.data().about,
+          banner: docSnap.data().banner,
+        };
+        dispatch(setUser(currentUser));
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
     }
   });
 
