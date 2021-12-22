be23d2391ad34c7df3f5b6ea9586270a93b39572
Devlin <devlincodes@gmail.com>
2021-12-22 04:32:28 -0800

    Detach auth listeners; resetUserState if no auth

--- src/pages/channels/[sid]/[cid].tsx
+++ src/pages/channels/[sid]/[cid].tsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
 import Servers from "../../../components/Servers/Servers";
 import Channels from "../../../components/channels/Channels";
 import Chat from "../../../components/chat/Chat";
 import UserSettings from "../../../components/userSettings/UserSettings";
 import tw from "tailwind-styled-components/dist/tailwind";
-import { setUser } from "../../../features/user";
+import { setUser, resetUserState } from "../../../features/user";
 import { useAppDispatch } from "../../../redux/hooks";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
@@ -16,45 +17,52 @@ const Home: NextPage = () => {
   const { userSettingsOpen } = useSettingsState();
   const dispatch = useAppDispatch();
 
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
+  useEffect(() => {
+    const authStateListener = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
 
-      if (docSnap.exists()) {
-        const currentUser = {
-          username: docSnap.data().username,
+        if (docSnap.exists()) {
+          const currentUser = {
+            username: docSnap.data().username,
 
-          tag: docSnap.data().tag,
+            tag: docSnap.data().tag,
 
-          avatar: docSnap.data().avatar,
+            avatar: docSnap.data().avatar,
 
-          about: docSnap.data().about,
+            about: docSnap.data().about,
 
-          banner: docSnap.data().banner,
+            banner: docSnap.data().banner,
 
-          userID: user.uid,
-        };
+            userID: user.uid,
+          };
 
-        dispatch(setUser(currentUser));
-      } else {
-        // doc.data() will be undefined in this case
+          dispatch(setUser(currentUser));
+        } else {
+          // doc.data() will be undefined in this case
 
-        console.log("No such document!");
+          resetUserState();
+
+          console.log("No such document!");
+        }
       }
-    }
-  });
+    });
+    return () => {
+      authStateListener();
+    };
+  }, []);
 
   return (
     <Container>
       {userSettingsOpen ? (
         <UserSettings />
       ) : (
-        <Container>
+        <>
           <Servers />
           <Channels />
           <Chat />
-        </Container>
+        </>
       )}
     </Container>
   );
