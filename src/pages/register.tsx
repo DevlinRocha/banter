75b6536c8478bc3bcc84845cc10b3b9e04a9f611
Devlin <devlincodes@gmail.com>
2021-12-23 05:03:43 -0800

    Fixed not redirecting user upon signing up

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -16,33 +16,7 @@ export default function Register() {
   useEffect(() => {
     const authStateListener = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        const docRef = doc(db, "users", user.uid);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          const currentUser = {
-            username: docSnap.data().username,
-
-            tag: docSnap.data().tag,
-
-            avatar: docSnap.data().avatar,
-
-            about: docSnap.data().about,
-
-            banner: docSnap.data().banner,
-
-            userID: user.uid,
-          };
-
-          dispatch(setUser(currentUser));
-          router.push("/app");
-        } else {
-          // doc.data() will be undefined in this case
-
-          dispatch(resetUserState());
-
-          console.log("No such document!");
-        }
+        router.push("/channels/@me");
       } else {
         dispatch(resetUserState());
       }
