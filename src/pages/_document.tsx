96d52765ecdb994e74abbf6c254ce7b6025787e1
Devlin <devlincodes@gmail.com>
2021-12-29 17:48:29 -0800

    Created custom document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Banter</title>
+        <meta name="description" content="Banter is a Discord clone" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
