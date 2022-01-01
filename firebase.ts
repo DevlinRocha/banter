8e19e82a0b071f6890116b8aaf1dcffcd8cedff7
Devlin <devlincodes@gmail.com>
2021-12-31 21:03:57 -0800

    Refactored code

--- firebase.ts
+++ firebase.ts
@@ -71,6 +71,22 @@ export async function createAccount(
   }
 }
 
+async function updateUserDatabase(property: string, newValue: string) {
+  if (!auth.currentUser) return;
+
+  const user = auth.currentUser;
+
+  await setDoc(
+    doc(db, "users", user.uid),
+
+    {
+      [property]: newValue,
+    },
+
+    { merge: true }
+  );
+}
+
 export async function signIn(email: string, password: string) {
   try {
     const userCredential = await signInWithEmailAndPassword(
@@ -86,6 +102,17 @@ export async function signIn(email: string, password: string) {
   } catch (error) {
     console.error(error);
   }
+}
+
+async function reauthenticateUser(password: string) {
+  if (!auth.currentUser || !auth.currentUser.email) return;
+
+  const credential: AuthCredential = EmailAuthProvider.credential(
+    auth.currentUser.email,
+    password
+  );
+
+  await reauthenticateWithCredential(auth.currentUser, credential);
 }
 
 export async function logOut() {
@@ -110,27 +137,16 @@ export async function changeUsername(newUsername: string, password: string) {
   const user = auth.currentUser;
 
   try {
-    const credential: AuthCredential = EmailAuthProvider.credential(
-      auth.currentUser.email,
-      password
-    );
+    if (!user.displayName) return;
 
-    await reauthenticateWithCredential(user, credential);
+    await reauthenticateUser(password);
 
     await updateProfile(user, {
       displayName: newUsername,
     });
     // Profile updated!
 
-    await setDoc(
-      doc(db, "users", user.uid),
-
-      {
-        username: user.displayName,
-      },
-
-      { merge: true }
-    );
+    await updateUserDatabase("username", user.displayName);
   } catch (error) {
     console.error(error);
   }
@@ -139,28 +155,16 @@ export async function changeUsername(newUsername: string, password: string) {
 export async function changeEmail(newEmail: string, password: string) {
   if (!auth.currentUser || !auth.currentUser.email) return;
 
+  const user = auth.currentUser;
+
   try {
-    const credential: AuthCredential = EmailAuthProvider.credential(
-      auth.currentUser.email,
-      password
-    );
+    if (!user.email) return;
 
-    await reauthenticateWithCredential(auth.currentUser, credential);
-    // User re-authenticated
+    await reauthenticateUser(password);
 
-    await updateEmail(auth.currentUser, newEmail);
-    // Email updated
+    await updateEmail(user, newEmail);
 
-    await setDoc(
-      doc(db, "users", auth.currentUser.uid),
-
-      {
-        email: auth.currentUser.email,
-      },
-
-      { merge: true }
-    );
-    // Database updated
+    await updateUserDatabase("email", user.email);
   } catch (error) {
     console.error(error);
   }
