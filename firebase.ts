46bace759f0c2e6faed89c5dbf5625d8d0581532
Devlin <devlincodes@gmail.com>
2022-01-08 17:13:20 -0800

    Replaced firebaseConfig with environment variables

--- firebase.ts
+++ firebase.ts
@@ -25,13 +25,13 @@ import { UserData } from "./src/features/user";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWcZTM0vSyLz1hQeFbO2SwS2wB-hGHtlQ",
-  authDomain: "banter-69832.firebaseapp.com",
-  projectId: "banter-69832",
-  storageBucket: "banter-69832.appspot.com",
-  messagingSenderId: "441171355945",
-  appId: "1:441171355945:web:2a4136fc1e9c14fc7eef38",
-  measurementId: "G-YCR3GD7Y6V",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export async function createAccount(
