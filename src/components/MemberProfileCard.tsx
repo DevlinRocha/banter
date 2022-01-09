1d396553553235bea9515479d85c778b33819e84
Devlin <devlincodes@gmail.com>
2022-01-08 22:55:39 -0800

    Allowed highlighting username and about me section

--- src/components/MemberProfileCard.tsx
+++ src/components/MemberProfileCard.tsx
@@ -91,7 +91,7 @@ const StyledImage = tw(Image)`
 `;
 
 const UsernameContainer = tw.div`
-  pt-16 pb-4 pl-4 text-xl font-medium
+  pt-16 pb-4 pl-4 text-xl font-medium select-text
 `;
 
 const Username = tw.span`
@@ -110,4 +110,5 @@ const ProfileHeading = tw.h3`
 `;
 
 const AboutMeContainer = tw.div`
+  select-text
 `;
