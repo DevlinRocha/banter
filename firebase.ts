dad00342407b86f5c6e83bd9f6200c2f4acab2ec
Devlin <devlincodes@gmail.com>
2022-01-10 23:43:20 -0800

    Initial commit for voice channel functionality

--- firebase.ts
+++ firebase.ts
@@ -7,8 +7,11 @@ import {
   collection,
   updateDoc,
   getDoc,
-  DocumentData,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
   DocumentReference,
+  DocumentData,
 } from "firebase/firestore";
 import {
   getAuth,
@@ -225,6 +228,8 @@ export async function createServer(serverName: string) {
     "text"
   );
 
+  createChannel(serverDocRef.id, "General", "voice");
+
   await updateDefaultChannel(serverDocRef, defaultChannelRef);
 
   await joinServer(serverDocRef.id);
@@ -243,7 +248,93 @@ export async function createChannel(
     }
   );
 
+  if (type === "voice") {
+    createVoiceChannel(serverID, channelDocRef.id);
+  }
+
   return channelDocRef;
+}
+
+async function createVoiceChannel(serverID: string, channelID: string) {
+  const channelRef = doc(db, "servers", serverID, "channels", channelID);
+
+  console.log("Create PeerConnection with configuration: ", configuration);
+
+  const peerConnection = new RTCPeerConnection(configuration);
+
+  registerPeerConnectionListeners();
+
+  // Code for collecting ICE candidates below
+  const callerCandidatesCollection = collection(channelRef, "callerCandidates");
+
+  peerConnection.addEventListener(
+    "icecandidate",
+    async (event: RTCPeerConnectionIceEvent) => {
+      if (!event.candidate) {
+        console.log("Got final candidate!");
+        return;
+      }
+      console.log("Got candidate: ", event.candidate);
+      await addDoc(callerCandidatesCollection, event.candidate.toJSON());
+    }
+  );
+  // Code for collecting ICE candidates above
+
+  // Code for creating a room below
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  console.log("Created offer:", offer);
+
+  const roomWithOffer = {
+    offer: {
+      type: offer.type,
+      sdp: offer.sdp,
+    },
+  };
+
+  await setDoc(channelRef, roomWithOffer, { merge: true });
+  roomId = channelRef.id;
+  console.log(`New room created with SDP offer. Room ID: ${channelRef.id}`);
+  // Code for creating a room above
+
+  // Peer connection event listener, can try:
+  // const { remoteStream } = await openUserMedia();
+
+  // peerConnection.addEventListener("track", (event) => {
+  //   console.log("Got remote track:", event.streams[0]);
+  //   event.streams[0].getTracks().forEach(async (track) => {
+  //     console.log("Add a track to the remoteStream:", track);
+  //     remoteStream.addTrack(track);
+  //   });
+  // });
+
+  // Listening for remote session description below
+  // Needs to unsubscribe from listener when disconnecting from channel
+  const unsubscribe = onSnapshot(channelRef, async (snapshot) => {
+    const data = snapshot.data();
+    if (!peerConnection.currentRemoteDescription && data && data.answer) {
+      console.log("Got remote description: ", data.answer);
+      const rtcSessionDescription = new RTCSessionDescription(data.answer);
+      await peerConnection.setRemoteDescription(rtcSessionDescription);
+    }
+  });
+  // Listening for remote session description above
+
+  // Listen for remote ICE candidates below
+
+  const unsubscribe2 = onSnapshot(
+    callerCandidatesCollection,
+    async (snapshot) => {
+      snapshot.docChanges().forEach(async (change) => {
+        if (change.type === "added") {
+          let data = change.doc.data();
+          console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
+          await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    }
+  );
+  // Listen for remote ICE candidates above
 }
 
 export async function updateDefaultChannel(
@@ -271,9 +362,189 @@ export async function joinServer(serverID: string) {
   await setDoc(doc(db, "users", user, "servers", serverID), {});
 }
 
+// VOICE CHAT FUNCTIONS
+
+export async function joinVoice(serverID: string, channelID: string) {
+  const { localStream, remoteStream } = await openUserMedia();
+
+  const channelRef = doc(db, "servers", serverID, "channels", channelID);
+  const channelSnapshot = await getDoc(channelRef);
+
+  console.log("Got room:", channelSnapshot.exists());
+
+  if (channelSnapshot.exists()) {
+    console.log("Create PeerConnection with configuration: ", configuration);
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    registerPeerConnectionListeners();
+
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
+
+    // Code for collecting ICE candidates below
+    const calleeCandidatesCollection = collection(
+      channelRef,
+      "calleeCandidates"
+    );
+
+    peerConnection.addEventListener("icecandidate", async (event) => {
+      if (!event.candidate) {
+        console.log("Got final candidate!");
+        return;
+      }
+      console.log("Got candidate: ", event.candidate);
+      await addDoc(calleeCandidatesCollection, event.candidate.toJSON());
+    });
+    // Code for collecting ICE candidates above
+
+    peerConnection.addEventListener("track", (event) => {
+      console.log("Got remote track:", event.streams[0]);
+      event.streams[0].getTracks().forEach((track) => {
+        console.log("Add a track to the remoteStream:", track);
+        remoteStream.addTrack(track);
+      });
+    });
+
+    // Code for creating SDP answer below
+
+    const offer = channelSnapshot.data().offer;
+    console.log("Got offer:", offer);
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnection.createAnswer();
+    console.log("Created answer:", answer);
+    await peerConnection.setLocalDescription(answer);
+
+    const roomWithAnswer = {
+      answer: {
+        type: answer.type,
+        sdp: answer.sdp,
+      },
+    };
+    await setDoc(channelRef, roomWithAnswer, { merge: true });
+    // Code for creating SDP answer above
+
+    const callerCandidatesCollection = collection(
+      channelRef,
+      "callerCandidates"
+    );
+
+    // Listening for remote ICE candidates below
+    const unsubscribe = onSnapshot(
+      callerCandidatesCollection,
+      async (snapshot) => {
+        snapshot.docChanges().forEach(async (change) => {
+          if (change.type === "added") {
+            let data = change.doc.data();
+            console.log(
+              `Got new remote ICE candidate: ${JSON.stringify(data)}`
+            );
+            await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+          }
+        });
+      }
+    );
+    // Listening for remote ICE candidates above
+  }
+}
+
+export async function openUserMedia() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: true,
+  });
+
+  const localStream = stream;
+  const remoteStream = new MediaStream();
+
+  return { localStream, remoteStream };
+}
+
+async function hangUp(serverID: string, channelID: string) {
+  // Add declaration for leave button
+
+  // const tracks = document.querySelector('#localVideo').srcObject.getTracks();
+  // tracks.forEach(track => {
+  // track.stop();
+  // });
+
+  // if (remoteStream) {
+  //   remoteStream.getTracks().forEach((track) => track.stop());
+  // }
+
+  if (peerConnection) {
+    peerConnection.close();
+  }
+
+  // Delete room on hangup
+  if (roomId) {
+    const channelRef = doc(db, "servers", serverID, "channels", channelID);
+
+    const calleeCandidatesCollection = collection(
+      channelRef,
+      "calleeCandidates"
+    );
+
+    const calleeCandidates = await getDocs(calleeCandidatesCollection);
+
+    calleeCandidates.forEach(async (candidate) => {
+      await deleteDoc(candidate.ref);
+    });
+
+    const callerCandidatesCollection = collection(
+      channelRef,
+      "callerCandidates"
+    );
+
+    const callerCandidates = await getDocs(callerCandidatesCollection);
+
+    callerCandidates.forEach(async (candidate) => {
+      await deleteDoc(candidate.ref);
+    });
+    await deleteDoc(channelRef);
+  }
+}
+
+function registerPeerConnectionListeners() {
+  peerConnection.addEventListener("icegatheringstatechange", () => {
+    console.log(
+      `ICE gathering state changed: ${peerConnection.iceGatheringState}`
+    );
+  });
+
+  peerConnection.addEventListener("connectionstatechange", () => {
+    console.log(`Connection state change: ${peerConnection.connectionState}`);
+  });
+
+  peerConnection.addEventListener("signalingstatechange", () => {
+    console.log(`Signaling state change: ${peerConnection.signalingState}`);
+  });
+
+  peerConnection.addEventListener("iceconnectionstatechange ", () => {
+    console.log(
+      `ICE connection state change: ${peerConnection.iceConnectionState}`
+    );
+  });
+}
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 const auth = getAuth();
 const user = auth.currentUser;
 export type User = typeof user;
 // export const analytics = getAnalytics(app);
+
+// VOICE CHAT DECLARATIONS
+
+const configuration = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+let peerConnection: RTCPeerConnection;
+let roomDialog: any = null;
+let roomId: any = null;
