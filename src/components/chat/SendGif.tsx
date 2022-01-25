ddba0a48bb14fa6ab5d0e225a9e30da968a02250
Devlin <devlincodes@gmail.com>
2022-01-24 20:08:36 -0800

    Displays list of gifs instead of just first result

--- src/components/chat/SendGif.tsx
+++ src/components/chat/SendGif.tsx
@@ -1,8 +1,22 @@
 import { useEffect, useState } from "react";
 import tw from "tailwind-styled-components/dist/tailwind";
 
+interface GifData {
+  content_description: string;
+  id: string;
+  url: string;
+  media: [
+    {
+      loopedmp4: {
+        url: string;
+      };
+    }
+  ];
+}
+
 export default function SendGif() {
   const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState<GifData[]>([]);
   const [gifSrc, setGifSrc] = useState("");
 
   useEffect(() => {
@@ -13,6 +27,7 @@ export default function SendGif() {
     const baseURL = `https://g.tenor.com/v1/search?q=${searchInput}&key=${process.env.NEXT_PUBLIC_TENOR_API_KEY}&limit=8`;
     const fetchedGifs = await fetch(baseURL);
     const gifsData = await fetchedGifs.json();
+    setSearchResults(gifsData.results);
     setGifSrc(gifsData.results[0].media[0].loopedmp4.url);
   }
 
@@ -30,7 +45,19 @@ export default function SendGif() {
         </Header>
 
         <ContentContainer>
-          <Gif src={gifSrc} autoPlay loop preload="auto" />
+          <GifContainer>
+            {searchResults.map((result) => {
+              return (
+                <Gif
+                  src={result.media[0].loopedmp4.url}
+                  autoPlay
+                  loop
+                  preload="auto"
+                  key={result.id}
+                />
+              );
+            })}
+          </GifContainer>
         </ContentContainer>
       </GifPicker>
     </Container>
@@ -59,10 +86,15 @@ const GifSearch = tw.input`
 `;
 
 const ContentContainer = tw.div`
-  flex w-full h-full p-2
+  relative w-full h-full p-2 overflow-hidden
+  hover:overflow-y-scroll
+`;
+
+const GifContainer = tw.div`
+  absolute flex flex-wrap gap-2
 `;
 
 const Gif = tw.video`
-  w-fit h-fit max-w-[194px] max-h-[194px] rounded-[5px] object-cover cursor-pointer
+  w-[194px] h-fit rounded-[5px] object-cover cursor-pointer
   hover:outline hover:outline-[3px] hover:outline-primary hover:drop-shadow-md
 `;
