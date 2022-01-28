deb4646b7ed54529d57b09479724d698b112c090
Devlin <devlincodes@gmail.com>
2022-01-28 04:34:23 -0800

    Add viewMedia component

---
+++ src/components/ViewMedia.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import tw from "tailwind-styled-components/dist/tailwind";
+import { setViewMediaOpen, useServersState } from "../features/servers";
+import { useAppDispatch } from "../redux/hooks";
+
+export default function ViewMedia() {
+  const { viewMedia } = useServersState();
+  const dispatch = useAppDispatch();
+
+  function closeWindow() {
+    dispatch(setViewMediaOpen(false));
+  }
+
+  function stopPropagation(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+  }
+
+  return (
+    <Backdrop onClick={closeWindow}>
+      <Container onClick={stopPropagation}>
+        {viewMedia.type === "image" && viewMedia.src && (
+          <StyledImage
+            loader={() => viewMedia.src}
+            src={viewMedia.src}
+            layout="fill"
+          />
+        )}
+
+        {viewMedia.type === "video" && viewMedia.src && (
+          <StyledVideo src={viewMedia.src} autoPlay loop preload="auto" />
+        )}
+      </Container>
+    </Backdrop>
+  );
+}
+
+const Backdrop = tw.div`
+  fixed w-full h-full bg-black bg-opacity-[0.85] z-20
+`;
+
+const Container = tw.div`
+  fixed flex flex-col top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-110 bg-white rounded-md
+  h-full max-h-[609px]
+`;
+
+const StyledImage = tw(Image)`
+  object-cover
+`;
+
+const StyledVideo = tw.video`
+  object-cover
+`;
