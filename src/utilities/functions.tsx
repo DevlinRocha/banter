7857a2d4fae5bacdb8e1d335f9748946977163d0
Devlin <devlincodes@gmail.com>
2022-08-29 18:12:06 -0700

    Refactor utility functions

--- src/utilities/functions.tsx
+++ src/utilities/functions.tsx
@@ -3,76 +3,7 @@ import Link from "next/link";
 import { setChannel, useServersState } from "../features/servers";
 import { useAppDispatch } from "../redux/hooks";
 
-export function findLinks(message: string) {
-  const findChannels = useFindChannels;
-
-  if (
-    !message ||
-    (!message.includes("https://") && !message.includes("http://"))
-  )
-    return findChannels(message);
-
-  const messageArray = message.split(/(https?:\/\/\w[^ ]+)/);
-
-  const fixedArray = addSlash(messageArray);
-
-  return (
-    <>
-      {fixedArray.map((message, index) => {
-        return index % 2 === 0 ? (
-          <span key={index}>{findChannels(message)}</span>
-        ) : (
-          <Link href={message} passHref key={index}>
-            <LinkText rel="noreferrer noopener" target="_blank">
-              {findChannels(message)}
-            </LinkText>
-          </Link>
-        );
-      })}
-    </>
-  );
-}
-
-function addSlash(messageArray: string[]) {
-  return messageArray.map((message, index) => {
-    return index % 2 === 0
-      ? message
-      : message.includes("/", 8)
-      ? message
-      : message.concat("/");
-  });
-}
-
-function useFindChannels(message: string) {
-  const { channels } = useServersState();
-  const dispatch = useAppDispatch();
-
-  if (!message || !message.includes("#")) return message;
-
-  const messageArray = message.split(/(#\w[^ ]+)/);
-
-  return (
-    <>
-      {messageArray.map((message, index) => {
-        const match = channels.find(
-          (channel) => message.substring(1) === channel.name
-        );
-
-        return match ? (
-          <Link href={match.path} passHref key={index}>
-            <ChannelLinkText onClick={() => dispatch(setChannel(match))}>
-              {message}
-            </ChannelLinkText>
-          </Link>
-        ) : (
-          <span key={index}>{message}</span>
-        );
-      })}
-    </>
-  );
-}
-
-export function findURLs(message: string) {
+export function parseURLs(message: string) {
   if (
     !message ||
     (!message.includes("https://") && !message.includes("http://"))
@@ -94,6 +25,45 @@ export function findURLs(message: string) {
               {message}
             </LinkText>
           </Link>
+        );
+      })}
+    </>
+  );
+}
+
+function addSlash(messageArray: string[]) {
+  return messageArray.map((message, index) => {
+    return index % 2 === 0
+      ? message
+      : message.includes("/", 8)
+      ? message
+      : message.concat("/");
+  });
+}
+
+export function useParseLinks(message: string) {
+  const { channels } = useServersState();
+  const dispatch = useAppDispatch();
+
+  if (!message || !message.includes("#")) return parseURLs(message);
+
+  const messageArray = message.split(/(#\w[^ ]+)/);
+
+  return (
+    <>
+      {messageArray.map((message, index) => {
+        const match = channels.find(
+          (channel) => message.substring(1) === channel.name
+        );
+
+        return match ? (
+          <Link href={match.path} passHref key={index}>
+            <ChannelLinkText onClick={() => dispatch(setChannel(match))}>
+              {parseURLs(message)}
+            </ChannelLinkText>
+          </Link>
+        ) : (
+          <span key={index}>{parseURLs(message)}</span>
         );
       })}
     </>
