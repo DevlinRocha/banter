d548420f22259ce7e235cf65a36030533d2762ca
Devlin <devlincodes@gmail.com>
2022-12-27 19:42:52 -0800

    Use dynamic SVG components to expand dark theme

--- src/components/userSettings/UserSettingsSidebar.tsx
+++ src/components/userSettings/UserSettingsSidebar.tsx
@@ -1,8 +1,7 @@
 import tw from "tailwind-styled-components/dist/tailwind";
-import Image from "next/image";
-import twitterIcon from "../../../assets/twitterIcon.svg";
-import githubIcon from "../../../assets/githubIcon.svg";
-import instagramIcon from "../../../assets/instagramIcon.svg";
+import TwitterIcon from "./TwitterIcon";
+import GithubIcon from "./GithubIcon";
+import InstagramIcon from "./InstagramIcon";
 import { useAppDispatch } from "../../redux/hooks";
 import {
   setLogoutConfirmOpen,
@@ -104,21 +103,21 @@ export default function SettingsSidebar() {
 
         <SocialLinks>
           <SocialLink href="https://twitter.com/DevlinRocha" target="_blank">
-            <StyledImage src={twitterIcon} width={16} height={16} />
+            <Twitter />
           </SocialLink>
 
           <SocialLink
             href="https://github.com/DevlinRocha/banter"
             target="_blank"
           >
-            <StyledImage src={githubIcon} width={16} height={16} />
+            <Github />
           </SocialLink>
 
           <SocialLink
             href="https://www.instagram.com/devlinrocha/"
             target="_blank"
           >
-            <StyledImage src={instagramIcon} width={16} height={16} />
+            <Instagram />
           </SocialLink>
         </SocialLinks>
       </NavContainer>
@@ -170,6 +169,12 @@ const SocialLinks = tw(SettingsList)`
 const SocialLink = tw.a`
   px-0.5 mr-2
 `;
-const StyledImage = tw(Image)`
-  object-contain
+
+const Twitter = tw(TwitterIcon)`
+`;
+
+const Github = tw(GithubIcon)`
+`;
+
+const Instagram = tw(InstagramIcon)`
 `;
